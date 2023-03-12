17a75b6f52ce20ed1ad8ca542a26a06af6f9dfa9
Buda Samir <Abdulrahman.samir.attia.elseed.96@gmail.com>
2023-03-12 14:43:52 +0200

    Created base URL and Api Key

--- weather_app_one/lib/services/weather_service.dart
+++ weather_app_one/lib/services/weather_service.dart
@@ -3,9 +3,11 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class WeatherServices {
+  String baseUrl = "http://api.weatherapi.com/v1";
+  String apiKey = "0a7e509d571c4b51920115629231203";
+
   void getWeather({required String city}) async {
-    Uri url = Uri.parse(
-        "http://api.weatherapi.com/v1/current.json?key=0a7e509d571c4b51920115629231203&q=$city&aqi=no");
+    Uri url = Uri.parse("$baseUrl/forecast.json?key=$baseUrl&q=$city&days=7");
     http.Response response = await http.get(url);
 
     Map<String, dynamic> data = jsonDecode(response.body);
