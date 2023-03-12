dfb5bf4848318c611d1c8d5c9937cba0cce04bac
Buda Samir <Abdulrahman.samir.attia.elseed.96@gmail.com>
2023-03-12 14:30:09 +0200

    create jsondecode and translated into data (MAP)

--- weather_app_one/lib/services/weather_service.dart
+++ weather_app_one/lib/services/weather_service.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 
 class WeatherServices {
@@ -5,5 +7,7 @@ class WeatherServices {
     Uri url = Uri.parse(
         "http://api.weatherapi.com/v1/current.json?key=0a7e509d571c4b51920115629231203&q=$city&aqi=no");
     http.Response response = await http.get(url);
+
+    Map<String, dynamic> data = jsonDecode(response.body);
   }
 }
